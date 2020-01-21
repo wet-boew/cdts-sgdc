cce64446742ef2ca1014b06b4db487235dfc698a
ipaksc <ilya.pak@servicecanada.gc.ca>
2020-01-21 13:02:55 -0500

    Updated gruntfile.js

--- gruntfile.js
+++ gruntfile.js
@@ -10,11 +10,16 @@ module.exports = function(grunt) {
 					'dist/cdts/js/exitScript.min.js' : ['_src/js/exitScript.js']
 				}
 			}
+		},
+		run: {
+		  npm_travis_script: {
+			exec: 'npm run travis_script --silent'
+		  }
 		}
-
 	});
 	//Loading Grunt plugins and tasks
 	require('load-grunt-tasks')(grunt);
+	grunt.loadNpmTasks('grunt-run');
 	//Custom tasks
-	grunt.registerTask('default', ["uglify"]);
+	grunt.registerTask('default', ['run:npm_travis_script', 'uglify']);
 };
