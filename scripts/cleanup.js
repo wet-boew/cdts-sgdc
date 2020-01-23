6ed076ed22a2216aa224f19577020aebc973b256
Shawn Thompson <plansmash@me.com>
2020-01-23 11:44:00 -0500

    removes old scripts that were replaced with grunt

--- scripts/cleanup.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs-extra')
-
-// Delete tmp folder after compile is done
-fs.remove('./tmp', err => {
-    if (err) return console.error(err)
-  
-    console.log('tmp deleted!')
-  })
-
