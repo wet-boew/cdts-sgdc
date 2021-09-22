bcd633291d301b6454b6add8f0931a02688529d2
Pierre Lupien <pierre.lupien@hrsdc-rhdcc.gc.ca>
2021-09-22 10:56:13 -0400

    build: Added a 'build-nowet' grunt task to allow faster builds on esdc workstations

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,25 +8,25 @@ const {compileEJSModule, extractEJSModuleMessages, mergeLanguageFiles} = require
 ///
 /// ************************************************************
 module.exports = function(grunt) {
-    
+
     //---[ Content replacing function (in copy and concat tasks)
     //(replaces CDTS version mentions in URLs and optionally environment in sample pages.)
     function cdtsContentReplace(content, srcpath) {
         const newVersionName = grunt.config('project.version_name');
         const newEnvironment = grunt.option('cdts_samples_cdnenv') || null;
-        
+
         //Replace version...
         var vtr = content.replace(/\/v[0-9]+_[0-9]+_[0-9]+\//g, `/${newVersionName}/`); //replaces '/vX_X_X/' where X can be any number
-        
+
         //Replace environment in sample pages...
         if (newEnvironment && (srcpath.includes('/samples/') || srcpath.includes('/appTop/'))) {
             vtr = vtr.replace(/"cdnEnv": "esdcprod"/g, `"cdnEnv": "${newEnvironment}"`);
             vtr = vtr.replace(/"cdnEnv": "prod"/g, `"cdnEnv": "${newEnvironment}"`);
         }
-        
+
         return vtr;
     }
-    
+
     //---[ Grunt Modules
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-copy');
@@ -36,7 +36,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-webdriver');
-    
+
     //---[ Task Definitions
     grunt.registerTask('default', 'Default task (performs a dev build)', ['build']);
     grunt.registerTask('build', 'Run non-minified build', ['clean', 'copy-public', 'build-ejs', 'genstatic']);
@@ -44,15 +44,20 @@ module.exports = function(grunt) {
     grunt.registerTask('copy-test', 'Copy all test files', ['copy:gcweb-test', 'copy:gcintranet-test']);
     grunt.registerTask('build-ejs', 'Produce Javascript from EJS templates', ['i18n-ejs', 'compile-ejs', 'concat']);
     grunt.registerTask('build-prod', 'Run production build', ['build', 'minify']);
+	grunt.registerTask('build-nowet', 'Run build without clean:target, copy-wet and genstatic (for convenience because of McAfee performance)', ['nowet-warning', 'clean:temp', 'copy:gcweb-public', 'copy:gcintranet-public', 'copy:global-public', 'build-ejs']);
     grunt.registerTask('minify', 'Minify target files', ['uglify']);
-    
+
     grunt.registerTask('serve', 'Start development web server', ['build', 'copy-test', 'connect', 'watch']);
     grunt.registerTask('serve-nobuild', 'Start development web server on current build (USE WITH CAUTION, only use with known state of directories dist and tmp)', ['nobuild-warning', 'connect', 'watch']);
-    grunt.registerTask('test', 'Start dev web server and run tests', ['setenv', 'build-prod', 'copy-test', 'connect', 'webdriver:maintests']); //NOTE: should we do a build-prod instead?
+    grunt.registerTask('test', 'Start dev web server and run tests', ['setenv', 'build-prod', 'copy-test', 'connect', 'webdriver:maintests']);
 
     grunt.registerTask('nobuild-warning', 'Issue a warning on screen about using serve-nobuild', function() {
         grunt.log.writeln('***** WARNING ***** When using "serve-nobuild", you have to be sure that the directories "dist" and "tmp" are in a known good state (as they would be after a build)');
         grunt.log.writeln('                If ./dist and ./tmp are not consistent with a proper build, you may experience unexpected runtime errors.');
+    });
+    grunt.registerTask('nowet-warning', 'Issue a warning on screen about using build-nowet', function() {
+        grunt.log.writeln('***** WARNING ***** When using "build-nowet", the "dist" directory is not cleaned, the WET files are not copied to "dist" and the static files are not re-generated (as they would be with a build).');
+        grunt.log.writeln('                "build-nowet" IS FOR CONVENIENCE/TROUBLESHOOTING ONLY AND MUST NOT BE USED IN THE CONTEXT OF A CI/CD PIPELINE.');
     });
 
     grunt.registerTask('setenv', 'Set environment variable from grunt configuration', function(target) {
@@ -64,7 +69,7 @@ module.exports = function(grunt) {
     //---[ Can get called with 'compile-ejs', 'compile-ejs:gcweb' or 'compile-ejs:gcintranet'
     grunt.registerTask('compile-ejs', 'Compile EJS templates', function(target) {
         const projectTempDir = grunt.config('project.temp');
-        
+
         ['gcweb', 'gcintranet'].forEach((themeName) => {
             //(if target specified, only run for that one)
             if ( (!target) || (themeName === target) ) {
@@ -78,7 +83,7 @@ module.exports = function(grunt) {
                 //convertXliffToJSON(`./src/${themeName}/wet-messages.fr.xlf`, `./src/${themeName}/wet-messages-conv.fr.json`);
             }
         });
-        
+
         return true;
     });
 
@@ -91,36 +96,36 @@ module.exports = function(grunt) {
                 mergeLanguageFiles(`./src/${themeName}`, 'en', ['fr'], false, true);
             }
         });
-        
+
         return true;
     });
-    
+
     //---[ Can get called with 'genstatic', 'genstatic:gcweb' or 'genstatic:gcintranet'
     grunt.registerTask('genstatic', 'Generate static fallback files.', function(target) {
         const fs = require('fs');
         const path = require('path');
         const definitionPath = './src/fallbackFileDefinitions';
-        
+
         grunt.log.writeln('--- Generating static fallback files...', target || '<all>');
-        
+
         ['gcweb', 'gcintranet'].forEach((themeName) => {
             //(if target specified, only run for that one)
             if ( (!target) || (themeName === target) ) {
                 const files = fs.readdirSync(definitionPath);
-                
+
                 grunt.log.writeln(`---   ${themeName}: Processing ${files.length} definition file(s)...`);
-                
+
                 files.forEach((fallbackFile) => {
                     const fallbackFileDefPath = `${definitionPath}/${fallbackFile}`;
-                    
+
                     const getStaticFileDefinition = require(fallbackFileDefPath);
-                    
+
                     generateStaticFile(grunt, themeName, path.basename(fallbackFile, path.extname(fallbackFile)), getStaticFileDefinition);
                 });
             }
         });
     });
-    
+
     //---[ Configuration
     grunt.util.linefeed = '\n';
     grunt.initConfig({
@@ -133,7 +138,7 @@ module.exports = function(grunt) {
             banner:  '/*!\n * Centrally Deployed Templates Solution (CDTS) / Solution de gabarits à déploiement centralisé (SGDC)\n' +
                         ' * Version <%= project.pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n *\n */',
         },
-        
+
         clean: {
             target: '<%= project.target %>',
             temp: '<%= project.temp %>',
@@ -186,7 +191,7 @@ module.exports = function(grunt) {
                 },
             },
         },
-        
+
         concat: {
             options: {
                 banner: '<%= project.banner %>',
@@ -210,7 +215,7 @@ module.exports = function(grunt) {
                 dest: '<%= project.target %>/gcintranet/<%= project.version_name %>/cdts/compiled/wet-fr.js',
             },
         },
-        
+
         uglify: {
             options: {
                 sourceMap: true,
@@ -232,7 +237,7 @@ module.exports = function(grunt) {
                 expand:true,
             },
         },
-  
+
         connect: {
             server: {
                 options: {
@@ -284,11 +289,11 @@ module.exports = function(grunt) {
                 tasks: ['compile-ejs:gcintranet', 'concat:gcintranet-fr', 'genstatic:gcintranet'],
             },
         },
-        
+
         webdriver: {
             maintests: {
                 configFile: './wdio.conf.js',
             },
         },
-    });     
+    });
 };
