bb534fb31bb434c002f41914da51a12e74a6818f
Pierre Lupien <pierre.lupien@hrsdc-rhdcc.gc.ca>
2021-11-04 13:01:13 -0400

    Adding generation of SRI hashes for public files

--- SRIUtilities.js
+++ SRIUtilities.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const path = require('path');
 const crypto = require('crypto');
 
 //Write the output file with the file names and their hashes
@@ -7,6 +8,12 @@ module.exports.writeFilesSRIHashes = function writeFilesSRIHashes(inputJSONFileN
     const files = JSON.parse(sriFileList);
     const hashes = module.exports.getSRIHashes(files);
 
+    //create target directory if it doesn't exist
+    const distStaticTargetDirName = path.dirname(outputJSONFileName);
+    if (!fs.existsSync(distStaticTargetDirName)) fs.mkdirSync(distStaticTargetDirName, { recursive: true });
+
+    //write output to file
+    console.log(`Writing SRI hash map to: ${outputJSONFileName}`);
     fs.writeFileSync(outputJSONFileName, JSON.stringify(hashes), {encoding: 'utf8'});
 }
 
