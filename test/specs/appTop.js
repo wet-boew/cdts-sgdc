c0fc7b6fd729be95ae96f2e6a8576246feac307f
Ahmad <52757738+ahmad-shahid@users.noreply.github.com>
2021-11-17 09:44:16 -0500

    Fixing accessibility issue on menu bar (#327)
    * Fixing accessibility issue on menu bar
    
    * Updating role for menu/submenu items

--- test/specs/appTop.js
+++ test/specs/appTop.js
@@ -166,6 +166,8 @@ describe('AppTop section tests for GCWeb', () => {
     it('Accessibility', async () => {
         await accessibility(theme, 'en');
         await accessibility(theme, 'fr');
+        await accessibility(theme, 'en', 'externalLinkMenu');
+        await accessibility(theme, 'fr', 'externalLinkMenu');
     });
 });
 
@@ -198,7 +200,7 @@ describe('AppTop section tests for GCIntranet', () => {
 
     generateTestFile('./test/html/gcintranet/template-gcintranetapp-fr.html', 'gcintranet', 'gcintranet-appTop-externalLinkMenu-fr', {
         refTop: '{"cdnEnv": "localhost"}',
-        appTop: '{"cdnEnv": "localhost", "GCToolsModal": true, "appName": [{"text": "Application name", "href": "#", "acronym": "Acronym"}], "lngLinks": [{"lang": "en", "href": "gcintranet-appTop-en.html", text: "English"}], "menuPath": "../ajax/custommenu-eng.html"}',
+        appTop: '{"cdnEnv": "localhost", "GCToolsModal": true, "appName": [{"text": "Application name", "href": "#", "acronym": "Acronym"}], "lngLinks": [{"lang": "en", "href": "gcintranet-appTop-en.html", text: "English"}], "menuPath": "../ajax/custommenu-fra.html"}',
         preFooter: '{"cdnEnv": "localhost"}',
         appFooter: '{"cdnEnv": "localhost"}',
         refFooter: '{"cdnEnv": "localhost"}'
@@ -420,6 +422,8 @@ describe('AppTop section tests for GCIntranet', () => {
     it('Accessibility', async () => {
         await accessibility(theme, 'en');
         await accessibility(theme, 'fr');
+        await accessibility(theme, 'en', 'externalLinkMenu');
+        await accessibility(theme, 'fr', 'externalLinkMenu');
     });
 });
 
@@ -741,7 +745,7 @@ async function defaultMenu(theme, lang){
     await expect(basicPage.defaultMenu).toExist();
 }
 
-async function accessibility(theme, lang) {
-    await appTopPage.open(theme, lang);
+async function accessibility(theme, lang , classifier = '') {
+    await appTopPage.open(theme, lang, classifier);
     await runAccessbilityTest();
 }
