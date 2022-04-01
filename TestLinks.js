205e1fb5d32c7dc514d4569d58f03b4edfd50860
j-paquette <56076749+j-paquette@users.noreply.github.com>
2022-04-01 15:25:39 -0400

    still wip, fix await validateLinks error

--- TestLinks.js
+++ TestLinks.js
@@ -87,6 +87,7 @@ module.exports.testFileLinks = async function testFileLinks(directories, excepti
     let skippedSyntaxUrlCount = 0;
     let skippedIntranetUrlCount = 0;
     let successResponseUrlCount = 0;
+    let skippedBinaryFileCount = 0;
 
     process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0; //eslint-disable-line
 
@@ -94,118 +95,117 @@ module.exports.testFileLinks = async function testFileLinks(directories, excepti
     function getFiles(dir, inFiles) {
         const myFiles = inFiles || [];
         const files = fs.readdirSync(dir);
-         
+
         for (const i in files) {
             const name = dir + '/' + files[i];
 
             if (fs.statSync(name).isDirectory()) {
                 getFiles(name, myFiles);
             }
+            else if (!isBinaryFileSync(name)) {
+                myFiles.push(name);
+            }
             else {
-
-                if (!isBinaryFileSync(name)) {
-                    myFiles.push(name);
-                  }
-                  else {
-                    
-                  }
-                
+                skippedBinaryFileCount++;
+                continue;
             }
         }
-        return myFiles;
     }
+    return myFiles;
+}
 
-    //Check response - regex determines if response starts with '2' or '3' followed by 2 digits, or is 401 (this will be considered valid)
-    function checkAxiosResponse(res, validURL) {
-        if ((/([23][\d]{2})|401/).test(res.status) === false) {
-            console.error("Error: " + validURL.href + "does not have a valid response. Status: " + res.status);
+//Check response - regex determines if response starts with '2' or '3' followed by 2 digits, or is 401 (this will be considered valid)
+function checkAxiosResponse(res, validURL) {
+    if ((/([23][\d]{2})|401/).test(res.status) === false) {
+        console.error("Error: " + validURL.href + "does not have a valid response. Status: " + res.status);
+        errorCount++;
+    }
+    else {
+        successResponseUrlCount++;
+    }
+}
+
+async function validateLinks(urls) {
+    for (let i = 0; i < urls.length; i++) {
+        //Check if URL is in the exception list of syntax check and validation
+        if (exceptionSyntaxLinks.some((l) => urls[i].startsWith(l))) {
+            skippedSyntaxUrlCount++;
+            continue;
+        }
+
+        //Issue error message if URL starts with HTTP unless it is part of the HTTP exception list
+        if (urls[i].startsWith("http:/") && !exceptionHTTPLinks.some((l) => urls[i].startsWith(l))) {
+            console.error("Error: HTTP URL Found: " + urls[i]);
             errorCount++;
         }
         else {
-            successResponseUrlCount++;
-        }
-    }
+            try {
+                const validURL = new URL(urls[i]);
+                //We can choose to skip testing for intranet links
+                if (process.env.DISABLE_PROXY && ((validURL.host).endsWith('.prv') || exceptionIntranetLinks.some((l) => validURL.href.startsWith(l)))) {
+                    skippedIntranetUrlCount++;
+                    continue;
+                }
 
-    async function validateLinks(urls) {
-        for (let i = 0; i < urls.length; i++) {
-            //Check if URL is in the exception list of syntax check and validation
-            if (exceptionSyntaxLinks.some((l) => urls[i].startsWith(l))) {
-                skippedSyntaxUrlCount++;
-                continue;
-            }
-
-            //Issue error message if URL starts with HTTP unless it is part of the HTTP exception list
-            if (urls[i].startsWith("http:/") && !exceptionHTTPLinks.some((l) => urls[i].startsWith(l))) {
-                console.error("Error: HTTP URL Found: " + urls[i]);
-                errorCount++;
-            }
-            else {
                 try {
-                    const validURL = new URL(urls[i]);
-                    //We can choose to skip testing for intranet links
-                    if (process.env.DISABLE_PROXY && ((validURL.host).endsWith('.prv') || exceptionIntranetLinks.some((l) => validURL.href.startsWith(l)))) {
-                        skippedIntranetUrlCount++;
-                        continue;
-                    }
-
+                    const res = await axios.head(validURL.href, config); //eslint-disable-line no-await-in-loop
+                    checkAxiosResponse(res, validURL);
+                }
+                catch (headErr) {
+                    console.warn("Warning: " + validURL.href + " failed with a HEAD request. Retrying with a GET request. The error was: " + headErr);
                     try {
-                        const res = await axios.head(validURL.href, config); //eslint-disable-line no-await-in-loop
+                        const res = await axios.get(validURL.href, config); //eslint-disable-line no-await-in-loop
                         checkAxiosResponse(res, validURL);
                     }
-                    catch (headErr) {
-                        console.warn("Warning: " + validURL.href + " failed with a HEAD request. Retrying with a GET request. The error was: " + headErr);
-                        try {
-                            const res = await axios.get(validURL.href, config); //eslint-disable-line no-await-in-loop
-                            checkAxiosResponse(res, validURL);
-                        }
-                        catch (getErr) {
-                            console.error("Error: " + validURL.href + " encountered the following error: " + getErr.message);
-                            errorCount++;
-                        }
+                    catch (getErr) {
+                        console.error("Error: " + validURL.href + " encountered the following error: " + getErr.message);
+                        errorCount++;
                     }
                 }
-                catch (err) {
-                    console.error("Error: An error occured with URL: " + urls[i] + " " + err);
-                    errorCount++;
-                }
+            }
+            catch (err) {
+                console.error("Error: An error occurred with URL: " + urls[i] + " " + err);
+                errorCount++;
             }
         }
-        return errorCount;
     }
+    return errorCount;
+}
 
-    if (process.env.DISABLE_TESTLINKS) return;
+if (process.env.DISABLE_TESTLINKS) return;
 
-    const startTime = performance.now();
-    console.log("***** Scanning directories: " + directories);
-    let files = [];
-    directories.forEach((directory) => {
-        files = files.concat(getFiles(directory));
-    });
+const startTime = performance.now();
+console.log("***** Scanning directories: " + directories);
+let files = [];
+directories.forEach((directory) => {
+    files = files.concat(getFiles(directory));
+});
 
-    console.log("***** Reading files and scanning for links");
-    for (let i = 0; i < files.length; i++) {
-        const fileMatches = fs.readFileSync(files[i], 'utf8').match(regex);
-        if (fileMatches !== null) matches = matches.concat(fileMatches);
+console.log("***** Reading files and scanning for links");
+for (let i = 0; i < files.length; i++) {
+    const fileMatches = fs.readFileSync(files[i], 'utf8').match(regex);
+    if (fileMatches !== null) matches = matches.concat(fileMatches);
+}
+
+const urls = [...new Set(matches)];
+
+console.log("***** Validating links");
+const totalErrorCount = await validateLinks(urls);
+const endTime = performance.now();
+console.log(`Done, validating all the links took ${endTime - startTime} milliseconds.`);
+console.log(urls.length + " unique URLs were found.");
+console.log(successResponseUrlCount + " URLs had a successful response.");
+console.log(skippedSyntaxUrlCount + " URLs were skipped because of their syntax.");
+console.log(skippedIntranetUrlCount + " URLs were skipped because they are intranet links.");
+console.log(skippedBinaryFileCount + " URLs were skipped because they are binary files.");
+
+if (totalErrorCount !== 0) {
+    console.error("Error: " + totalErrorCount + " error(s) were found when validating " + urls.length + " URLs.");
+    if (!process.env.DISABLE_TESTLINKS_THROWONFAIL) {
+        throw new Error("Error: " + totalErrorCount + " error(s) were found when validating" + urls.length + " URLs.");
     }
-
-    const urls = [...new Set(matches)];
-
-    console.log("***** Validating links");
-    const totalErrorCount = await validateLinks(urls);
-    const endTime = performance.now();
-    console.log(`Done, validating all the links took ${endTime - startTime} milliseconds.`);
-    console.log(urls.length + " unique URLs were found.");
-    console.log(successResponseUrlCount + " URLs had a successful response.");
-    console.log(skippedSyntaxUrlCount + " URLs were skipped because of their syntax.");
-    console.log(skippedIntranetUrlCount + " URLs were skipped because they are intranet links.");
-
-    if (totalErrorCount !== 0) {
-        console.error("Error: " + totalErrorCount + " error(s) were found when validating " + urls.length + " URLs.");
-        if (!process.env.DISABLE_TESTLINKS_THROWONFAIL) {
-            throw new Error("Error: " + totalErrorCount + " error(s) were found when validating" + urls.length + " URLs.");
-        }
-    }
-    else {
-        console.log("TestLinks Completed Successfully.");
-    }
+}
+else {
+    console.log("TestLinks Completed Successfully.");
+}
 }
