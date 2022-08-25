1d2d84e7fc1e0efbf30b509f136dd00318421d08
ahmad.shahid <ahmad.shahid@hrsdc-rhdcc.gc.ca>
2022-08-25 13:05:21 -0400

    Updating attribute-allowed-values rule to warning

--- htmlvalidator.conf.js
+++ htmlvalidator.conf.js
@@ -10,6 +10,9 @@ module.exports = {
         "no-conditional-comment": "off",
         "prefer-native-element": "off",
         "element-permitted-content": "warn",
-        "no-inline-style": "off"
+        "no-inline-style": "off",
+        //TODO: A temporary workaround for a bug that was introduced in html-validate (7.3.1)
+        //The validator needs to be fixed as it is throwing an error for a valid value for form action
+        "attribute-allowed-values": "warn"
     },
 };
