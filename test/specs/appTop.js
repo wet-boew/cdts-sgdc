59539d38038bbb6861318df473a306c38d9edf74
ahmad.shahid <ahmad.shahid@hrsdc-rhdcc.gc.ca>
2022-12-14 17:03:30 -0500

    Deleting extra lines

--- test/specs/appTop.js
+++ test/specs/appTop.js
@@ -205,8 +205,6 @@ describe('AppTop section tests for GCWeb', () => {
         await bannerAllConfigurations(theme, 'fr');
     });
 
-
-
     it('Accessibility', async () => {
         await accessibility(theme, 'en');
         await accessibility(theme, 'fr');
