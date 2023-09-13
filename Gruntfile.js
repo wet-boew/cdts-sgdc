95269b44d6a53d04619708881570511e8d260a7a
j-paquette <56076749+j-paquette@users.noreply.github.com>
2023-09-13 13:11:03 -0400

    create a new Gruntfile task test-nobuild (#952)
    * rebase changes
    
    * fixed to resolve comment

--- Gruntfile.js
+++ Gruntfile.js
@@ -97,9 +97,10 @@ module.exports = function run(grunt) {
     grunt.registerTask('serve', 'Start development web server', ['build', 'copy-test', 'connect', 'watch']);
     grunt.registerTask('serve-nobuild', 'Start development web server on current build (USE WITH CAUTION, only use with known state of directories dist and tmp)', ['nobuild-warning', 'connect', 'watch']);
     grunt.registerTask('test', 'Start dev web server and run tests', ['setenv', 'build-prod', 'copy-test', 'connect', 'webdriver:maintests', 'test-links']);
+    grunt.registerTask('test-nobuild', 'Run tests on current build (USE WITH CAUTION, only use with known state of directories dist and tmp)', ['nobuild-warning', 'setenv', 'copy-test', 'connect', 'webdriver:maintests', 'test-links']);
 
     grunt.registerTask('nobuild-warning', 'Issue a warning on screen about using serve-nobuild', () => {
-        grunt.log.writeln('***** WARNING ***** When using "serve-nobuild", you have to be sure that the directories "dist" and "tmp" are in a known good state (as they would be after a build)');
+        grunt.log.writeln('***** WARNING ***** When using "serve-nobuild" or "test-nobuild", you have to be sure that the directories "dist" and "tmp" are in a known good state (as they would be after a build)');
         grunt.log.writeln('                If ./dist and ./tmp are not consistent with a proper build, you may experience unexpected runtime errors.');
     });
     grunt.registerTask('nowet-warning', 'Issue a warning on screen about using build-nowet', () => {
