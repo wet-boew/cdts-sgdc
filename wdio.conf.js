6316ac0466cab109ecca3fe27023beb8f140f376
ahmad.shahid <ahmad.shahid@hrsdc-rhdcc.gc.ca>
2024-09-19 11:44:16 -0400

    Fixing browser flags

--- wdio.conf.js
+++ wdio.conf.js
@@ -82,7 +82,7 @@ exports.config = {
         'goog:chromeOptions': {
             // to run chrome headless the following flags are required
             // (see https://developers.google.com/web/updates/2017/04/headless-chrome)
-            args: ['--start-maximized', '-headless'], //['--disable-gpu'],
+            args: ['--start-maximized', '--headless', '--disable-gpu'],
         },
 
         acceptInsecureCerts: true,
