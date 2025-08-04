bc2fea207813fddedc8d1f6352e1e55e6cf4c02f
ahmad.shahid <ahmad.shahid@hrsdc-rhdcc.gc.ca>
2025-08-04 08:48:18 -0400

    Adding language precedence to splash page

--- test/pageobjects/splash.page.js
+++ test/pageobjects/splash.page.js
@@ -7,12 +7,12 @@ class SplashPage extends Page {
     /**
      * define selectors using getter methods
      */
-    get enBtnLink() { return $('a.btn-primary'); }
-    get frBtnLink() { return $('a.btn-default'); }
-    get enTermLink() { return $('div.col-xs-7 a:nth-of-type(1)'); }
-    get frTermLink() { return $('div.col-xs-7 a:nth-of-type(2)'); }
-    get enContent() { return $('div.cdts-splash section:first-child h2'); }
-    get frContent() { return $('div.cdts-splash section:last-child h2'); }
+    get primaryBtnLink() { return $('a.btn-primary'); }
+    get defaultBtnLink() { return $('a.btn-default'); }
+    get primaryTermLink() { return $('div.col-xs-7 a:nth-of-type(1)'); }
+    get defaultTermLink() { return $('div.col-xs-7 a:nth-of-type(2)'); }
+    get primaryContent() { return $('div.cdts-splash section:first-child h2'); }
+    get defaultContent() { return $('div.cdts-splash section:last-child h2'); }
     
     /**
     * Opens a sub page of the page
