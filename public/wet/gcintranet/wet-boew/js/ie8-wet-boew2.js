ac401d66ca4b58236673c72d7c555f41b23e19dc
ahmad.shahid <ahmad.shahid@hrsdc-rhdcc.gc.ca>
2025-09-26 14:35:17 -0400

    Revert WET to v4.0.85 for GCIntranet

--- public/wet/gcintranet/wet-boew/js/ie8-wet-boew2.js
+++ public/wet/gcintranet/wet-boew/js/ie8-wet-boew2.js
@@ -1,10 +1,10 @@
 /*!
  * Web Experience Toolkit (WET) / Boîte à outils de l'expérience Web (BOEW)
  * wet-boew.github.io/wet-boew/License-en.html / wet-boew.github.io/wet-boew/Licence-fr.html
- * v4.0.87 - 2025-03-19
+ * v4.0.85 - 2025-02-04
  *
  *//*
- * Web Experience Toolkit (WET) / Boîte à outils de l'expérience Web (BOEW)
+ * Web Experience Toolkit (WET) / Bo�te � outils de l'exp�rience Web (BOEW)
  * wet-boew.github.io/wet-boew/License-en.html / wet-boew.github.io/wet-boew/Licence-fr.html
  */
 /*! Deprecated */
